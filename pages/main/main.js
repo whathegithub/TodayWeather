0ccc9196993141907e17f1e01ecf4f052586c3a1
jiangbowen_ <18853716897@163.com>
2020-10-13 15:51:13 +0800

    增加网络请求

--- pages/main/main.js
+++ pages/main/main.js
@@ -5,15 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-    region: ["北京市", "北京市", "北京市"]
+    region: ["北京市", "北京市", "北京市"],
+    localCode : 0,
+    temp:0,
+    pa:0,
+    see:0,
+    wind_direction:0,
+    wind_speed: 0,
+    wind_power:0,
+    icon : 999
   },
 
   changeRegion: function(e){
     this.setData({
-      region: e.detail.value
+      region: e.detail.value,
+      localCode: e.detail.code
     })
-  },
+
+    this.getWeather()
+  }, 
  
+
+getWeather : function(){
+  var that = this ; //this不可以在wxApi内部使用
+  console.log(that.data.localCode);
+
+  wx.request({
+    url: 'https://geoapi.heweather.net/v2/city/lookup?',
+    data:{
+      location:that.data.region[1],
+      key:"51cee72b89814b628c1ee77957ea79de"
+    },
+
+    success:function(res){
+      wx.request({
+        url: 'https://devapi.heweather.net/v7/weather/now?',
+    
+        data:{
+          location: res.data.location[1].id,
+          key : "51cee72b89814b628c1ee77957ea79de"
+        },
+        success:function(res) {
+          console.log(res.data);
+          that.setData({
+            temp:res.data.now.temp,
+            wind_speed:res.data.now.windSpeed,
+            pa:res.data.now.pressure,
+            icon: res.data.now.icon
+          })
+        }
+      })
+    }
+  })
+  
+},
+
   /**
    * 生命周期函数--监听页面加载
    */
